b9795ac94f0d33e455630a92c1e6526d7cb03f9a
platais <domain@tvnet.lv>
2020-10-25 14:16:04 +0200

    Initial code

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static RTableServers host1List = new RTableServers();
+        static RTableServers host2List = new RTableServers();
+        static void Main(string[] args)
+        {
+            RTableServers servers = new RTableServers();
+
+            //10 total table servers we will ask for
+            for (int i = 0; i < 5; i++)
+            {
+                //Console.WriteLine($"The next server is: {servers.GetNextServer()}");
+                Host1GenNexServer();
+                Host2GenNexServer();
+            }
+            Console.ReadKey();
+        }
+        private static void Host1GenNexServer() 
+        {
+            Console.WriteLine($"The next h1 server is: {host1List.GetNextServer()}");
+        }
+        private static void Host2GenNexServer()
+        {
+            Console.WriteLine($"The next h2 server is: {host2List.GetNextServer()}");
+        }
+    }
+}
---
+++ RTableServers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class RTableServers
+    {
+        private List<string> servers = new List<string>();
+        private int nextServer = 0;
+        public RTableServers()
+        {
+            servers.Add("Arturs");
+            servers.Add("Janis");
+            servers.Add("Rob");
+        }
+
+        public string GetNextServer()
+        {
+            string output = servers[nextServer];
+            nextServer++;
+            //for constant rotation we add this
+            if (nextServer >= servers.Count)
+            {
+                nextServer = 0;
+            }
+            return output;
+        }
+
+    }
+}
