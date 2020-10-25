15fb166dc3b93efdf6530f380e96eecba8efd416
platais <domain@tvnet.lv>
2020-10-25 14:42:24 +0200

    comment

--- RTableServers.cs
+++ RTableServers.cs
@@ -13,6 +13,8 @@ namespace SingletonPattern
         //static - you can access _instance without class being extensiated(but now its private)
         //we create instance of this class only we start uzing it, kind of "lazy instantiation"
         //everyone gets the same instance
+        //but as long as your application keeps running this instance will stay open (problematic when on webserver)
+        //have to be selective when using singleton, maybe for som configuration thats available when app runs...
         private static readonly RTableServers _instance = new RTableServers();
         private List<string> servers = new List<string>();
         private int nextServer = 0;
